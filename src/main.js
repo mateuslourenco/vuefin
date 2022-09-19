03ba4dd2ecb67d03ee4b3a0cea30af5b8d03ee6a
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-09-19 20:27:30 -0300

    Added firebase config

--- src/main.js
+++ src/main.js
@@ -2,10 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import FirebaseVue from './firebase'
 const app = createApp(App);
 
 app.use(store);
 app.use(router);
-app.use(FirebaseVue)
+app.use(FirebaseVue);
 app.mount("#app");
