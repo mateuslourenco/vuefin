267ce34027a942a371cc466f999f804ce3920893
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-09-22 06:56:35 -0300

    Add logout btn

---
+++ src/components/layout/LayoutLogout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="menu-list btn-logout" @click="logout()">Sair</div>
+</template>
+
+<script>
+export default {
+  methods: {
+    async logout() {
+      await this.$store.dispatch("user/Logout");
+
+      this.$router.push({ name: "login" });
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.btn-logout {
+  color: white;
+  cursor: pointer;
+  &:hover {
+    color: orange;
+  }
+}
+</style>
--- src/components/layout/LayoutNavgation.vue
+++ src/components/layout/LayoutNavgation.vue
@@ -1,21 +1,32 @@
 <template>
-  <aside class="menu nav">
-    <p class="menu-label has-text-centered">VueFin</p>
-    <ul class="menu-list">
-      <router-link
-        :key="i"
-        exact-active-class="active"
-        class="nav-link"
-        :to="{ name: router.name }"
-        v-for="(router, i) in routerLinks"
-        >{{ router.meta.title }}</router-link
-      >
-    </ul>
+  <aside
+    class="menu nav is-flex is-flex-direction-column is-justify-content-space-between"
+  >
+    <div>
+      <p class="menu-label has-text-centered">VueFin</p>
+      <ul class="menu-list">
+        <router-link
+          :key="i"
+          exact-active-class="active"
+          class="nav-link"
+          :to="{ name: router.name }"
+          v-for="(router, i) in routerLinks"
+          >{{ router.meta.title }}</router-link
+        >
+      </ul>
+    </div>
+    <div class="has-text-centered">
+      <LayoutLogout></LayoutLogout>
+    </div>
   </aside>
 </template>
 
 <script>
+import LayoutLogout from "./LayoutLogout.vue";
 export default {
+  components: {
+    LayoutLogout,
+  },
   computed: {
     routerLinks() {
       return this.$router.options.routes.filter((r) => r.name != "login");
