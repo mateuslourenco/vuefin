fd67e98085f639c73dee3a7522ff5ffa96169999
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-09-23 04:18:33 -0300

    add vuetify

--- src/main.js
+++ src/main.js
@@ -10,6 +10,10 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
+import vuetify from "./plugins/vuetify";
+import { loadFonts } from "./plugins/webfontloader";
+
+loadFonts();
 dom.watch();
 library.add(fas, far, fab);
 
@@ -18,5 +22,6 @@ const app = createApp(App);
 app.use(store);
 app.use(database);
 app.use(router);
+app.use(vuetify);
 app.component("font-awesome-icon", FontAwesomeIcon);
 app.mount("#app");
