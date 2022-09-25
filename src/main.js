32c6f3b57aabdef0363c7d8b15ef7408210aeabf
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-09-25 17:04:17 -0300

    finished home

--- src/main.js
+++ src/main.js
@@ -12,12 +12,25 @@ import { fab } from "@fortawesome/free-brands-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
+import moment from "moment";
 
 loadFonts();
 dom.watch();
 library.add(fas, far, fab);
 
 const app = createApp(App);
+
+app.directive("money-format", (el, { value }) => {
+  el.innerHTML = new Intl.NumberFormat("pt-BR", {
+    currency: "BRL",
+    style: "currency",
+    minimumFractionDigits: 2,
+  }).format(value || 0);
+});
+
+app.directive("date-format", (el, { value }) => {
+  el.innerHTML = moment(value).format("DD/MM/YYYY [às] HH:mm:ss");
+});
 
 app.use(store);
 app.use(database);
