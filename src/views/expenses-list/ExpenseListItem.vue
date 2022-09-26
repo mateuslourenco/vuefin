5ffe2bc1d839949f0bdb4afff276b6d47e0ab647
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-09-26 07:17:10 -0300

    add current month

---
+++ src/views/expenses-list/ExpenseListItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="expense-list-item columns">
+    <div class="column is-one-fifth">10/02/2022</div>
+    <div class="column is-three-fifths">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    </div>
+    <div class="column is-one-fifth">R$ 100.00</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.expense-list-item {
+  border-bottom: 1px solid #ccc;
+}
+</style>
+>
